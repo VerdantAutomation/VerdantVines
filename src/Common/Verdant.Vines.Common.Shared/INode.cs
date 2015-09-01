cad76302fcb53ea07243a482e0b62175c67b575c
Martin Calsyn <martin@pervasive.digital>
2015-09-01 04:56:25 +0300

    clean up project structure

---
+++ src/Common/Verdant.Vines.Common.Shared/INode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Verdant.Vines.Common
+{
+    public delegate void NotifyPropertyChangedEventHandler(object sender, object foo);
+
+    interface INode : IDisposable
+    {
+        ServiceDescription ServiceDescription { get; }
+
+        void ExecuteMethod(string method, Hashtable inputs, Hashtable outputs);
+
+        event NotifyPropertyChangedEventHandler PropertyChanged;        
+    }
+}
---
+++ src/Common/Verdant.Vines.Common.Shared/ITransport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Verdant.Vines.Common
+{
+    public delegate void MessageReceivedEventHandler(object sender, Guid fromNodeId, string payload);
+
+    interface ITransport : IDisposable
+    {
+        void Open();
+        void Close();
+
+        bool IsConnected { get; }
+
+        void Send(Guid destinationNodeId, string payload);
+        void Send(Guid destinationNodeId, byte[] payload, int offset, int len);
+
+        event MessageReceivedEventHandler OnMessageReceived;
+    }
+}
